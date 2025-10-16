1a9f273662020b6abb3df201852ebd811a28419c
fullfinger <gyh2020163@163.com>
2025-10-16 20:03:28 +0800

    优化消息表实体接口的注释以增强可读性

--- src/entity/messagetable.entity.ts
+++ src/entity/messagetable.entity.ts
@@ -1,16 +1,18 @@
 // src/entity/messagetable.entity.ts
 export interface MessageTable {
-  channelId: number;
-  capacity: number;
-  threshold: number;
-  value: number;
-  messageIn: message[];
+  channelId: number;            // 频道ID
+  capacity: number;             // 最大容量
+  threshold: number;            // 触发阈值
+  value: number;                // 当前值
+  length: number;               // 已使用容量
+  messageIn: message[];         // 消息队列
+  lifeRest: number;             // 消息列表剩余寿命
 }
 
 export interface message {
-  id: number;
-  originator: number;
-  recipient: number;
-  content: string;
-  createdAt: Date;
+  id: number;                   // 消息ID（主要用来确定回复）
+  originator: number;           // 发送者ID
+  recipient: number;            // 接收者ID
+  content: string;              // 消息内容
+  createdAt: Date;              // 创建时间
 }
